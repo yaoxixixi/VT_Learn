58a49da909c56eb5aa9424e9f8f0c1dcd7bd8cd3
yaoxixixi <77949277+yaoxixixi@users.noreply.github.com>
2023-03-03 16:15:06 +0800

    Create entry.c

---
+++ myself/开锁和柜门/entry.c
@@ -0,0 +1,29 @@
+#include <ntddk.h>
+#include "vtsystem.h"
+
+VOID DriverUnload(PDRIVER_OBJECT driver)
+{
+
+    DbgPrint("Driver is unloading...\r\n");
+}
+
+
+
+NTSTATUS 
+  DriverEntry( 
+    PDRIVER_OBJECT  driver,
+    PUNICODE_STRING RegistryPath
+    )
+{
+    __asm int 3
+    DbgPrint("Driver Entered!\r\n");
+	driver->DriverUnload = DriverUnload;
+
+
+     
+
+
+	return STATUS_SUCCESS;
+}
+
+
